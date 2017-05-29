46bf1f041169a536859ec5dc7aedf1fbf04d8d6e
U137651 <U137651@54004p03x.ds.upf.edu>
2017-05-29 13:10:42 +0200

    hhh

---
+++ BinaryTree_template.h
@@ -0,0 +1,41 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+/*
+ * Tarea 1
+ * Especifique el tipo árbol de decisión binario
+ */
+typedef struct _BinaryTree {
+} BinaryTree;
+
+/*
+ * Tarea 2
+ * Implemente el árbol de decisión binario en C.
+ */
+BinaryTree* create_empty_tree();
+BinaryTree* create_tree(BinaryTree* left, char* content, BinaryTree* right); 
+BinaryTree* left_tree(BinaryTree* tree); 
+BinaryTree* right_tree(BinaryTree* tree); 
+char* get_content(BinaryTree* tree); 
+int is_empty(BinaryTree* tree);
+
+/*
+ * Tarea 3
+ * Implemente una función que dado un árbol de decisión binario imprima
+ *  de forma apropiada el árbol.
+ */
+void print_tree(BinaryTree* tree);
+
+/*
+ * Tarea 4
+ * Implemente un programa para leer, a partir de un fichero de texto,
+ * la estructura de un árbol de decisión binario y retorne
+ * el árbol instanciado apropiadamente.
+ */
+//Option A
+BinaryTree* create_example1();
+BinaryTree* create_example2();
+//Option B
+BinaryTree* create_tree_from_string(char* strTree);
+
+#endif
---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: U137651
+ *
+ * Created on 29 de mayo de 2017, 13:09
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
