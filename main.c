297b671963182622319b2eaa85b6aeb9c0fdb889
yeff28 <yeferson.ramirez.ryy@gmail.com>
2017-06-09 01:48:19 +0200

    Create main.c

--- main.c
+++ main.c
@@ -1,48 +1,49 @@
-/* 
- * File:   main.c
- * Author: U137651
- *
- * Created on 29 de mayo de 2017, 13:09
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "BinaryTree_template.h"
 
-/*
- * 
- */
 int main(int argc, char** argv) {
     BinaryTree* tree;
-   
-    tree = tree_names;
+    char buffer[200];
     int op;
-    do{
+    while(1){
+    //do{
+    op = -1;
+    printf("Elija una de las siguientes opciones\n");
     printf("(1)Mostrar arbol 1\n");
     printf("(2)Mostrar arbol 2\n");
     printf("(3)Interactuar con arbol 1\n");
     printf("(4)Interactuar con arbol 2\n");
     printf("(0)Exit\n");
-    printf("Que desea hacer?:");
-    scanf("%d",&op);
-    
+    if(scanf("%d",&op) != 1){                  //Error en el scanf
+           printf("Se ha introducido erroneamente la opcion\n");
+            scanf("%s", buffer);                            //Cogemos los valores de más que haya introducido el usuario
+       }
     switch(op){        
-        case 1: printf("Se muestra el arbol 1\n");
-        break;
-        case 2: printf("Se muestra el arbol 2\n");
-        break;
-        case 3: printf("Se interactua con el arbol 1\n");
-        break;
-        case 4: printf("Se interactua con el arbol 1\n");
-        break;
-        case 0: printf("\nAdios (╯°□°）╯︵ ┻━┻ \n");
-        break;
-        default: printf("El numero ingresado es erroneo\n");
+        case 1: 
+            tree = create_example1();
+            print_tree(tree);
+            break;
+        case 2: 
+            tree = create_example2();
+            print_tree(tree);
+            break;
+        case 3: 
+            print_manual_tree(tree);
+            break;
+        case 4: 
+            print_manual_tree(tree);
+            break;
+        case 0: 
+            printf("Gracias por utilizar el programa\n");
+            exit(0); 
+            break;
+        default: 
+            printf("El numero ingresado es erroneo\n");  
+    } 
     }
-                       
-    }while(op > 0 && op < 5 );
-    
-                       
+
+                     
     return (EXIT_SUCCESS);
 }
