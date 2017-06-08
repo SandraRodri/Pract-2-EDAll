daa15f7a545d3f4e886b097f37928815b705966d
mekakris <cj-ms1@hotmail.com>
2017-06-08 12:03:48 +0200

    Create constantes.h

---
+++ constantes.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   constantes.h
+ * Author: u140176
+ *
+ * Created on 8 de junio de 2017, 11:48
+ */
+
+#define TREE1_1 'Are you vegetarian?'
+#define TREE1_2 'Do you like tomatoes?'
+#define TREE1_3 'Do you like olive oil?'
+#define TREE1_4 'Do you like beef?'
+#define TREE1_5 'Do you like potatoes?'
+#define TREE1_6 'Do you like spicy food?'
+#define TREE1_8 'I suggest bread & tomatoes'
+#define TREE1_9 'I suggest tomato soup'
+#define TREE1_10 'I suggest green salad'
+#define TREE1_11 'I suggest grilled beef & french fries'
+#define TREE1_12 'I suggest gilled beef & tomato salad'
+#define TREE1_13 'I suggest chicken tikka'
+#define TREE1_14 'I suggest fried chicken'
+
+
+#define TREE2_1 'Is your age <= 16?'
+#define TREE2_2 'Is you age > 16 and <= 50?'
+#define TREE2_3 'Do you like suspense?'
+#define TREE2_4 'Do you like musical?'
+#define TREE2_5 'Do you like romance?'
+#define TREE2_6 'Do you like thrillers?'
+#define TREE2_8 'Do you like superheroes?'
+#define TREE2_9 'Watch Frozen'
+#define TREE2_10 'Watch Spiderman'
+#define TREE2_11 'Watch Shakerpeare in Love'
+#define TREE2_12 'Watch Fast an Furious 10'
+#define TREE2_13 'Watch the Davinci Code'
+#define TREE2_14 'Watch La La Land'
+#define TREE2_15 'Watch Back to Future'
